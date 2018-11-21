19b18cfa298917d250fd7dc8023c47d1485ad472
AluneCM <hello@alune.fr>
2018-11-21 18:20:45 +0100

    added lightbox effect to illustrations

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -35,3 +35,55 @@ window.addEventListener('scroll', function(){
 window.onload = function() {
   addShadow();
 };
+
+
+// Illu lightbox
+
+window.addEventListener("load", function() {
+
+  var thumbnail = document.querySelectorAll(".illu-thumbnail");
+  var fullImage = document.querySelectorAll(".illu-big");
+
+  function lockScroll() {
+    document.body.classList.add("locked");
+  }
+
+  function unlockScroll() {
+    document.body.classList.remove("locked");
+  }
+
+  function highlightImage(imageClickEvent) {
+
+    var clickedItem = imageClickEvent.currentTarget;
+    var itemID = clickedItem.getAttribute("illu-id");
+    var associatedHighlight = document.querySelector("#id" + itemID);
+
+    associatedHighlight.classList.add("active");
+
+    lockScroll();
+  };
+
+  function closeImage() {
+    for (var i = 0; i < fullImage.length; i++) {
+      fullImage[i].classList.remove("active");
+    }
+    unlockScroll();
+  }
+
+  for (i = 0; i < thumbnail.length; i++) {
+     thumbnail[i].addEventListener("click", function(e) {
+      closeImage();
+      highlightImage(e);
+     })
+  }
+
+  for (i = 0; i < fullImage.length; i++) {
+    fullImage[i].addEventListener("click", closeImage);
+  }
+});
+
+
+
+// illustration.addEventListener('click', function(e) {
+//   highlightIllu(e);
+// });
